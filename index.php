c1284c4a1ef33d6623cc646f4fbf50bc64fca4c4
Nard <theo.nard18@gmail.com>
2021-08-26 14:29:13 +0200

    indentation

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if ($url_parse[0] == "") //Nerf for title
 $result = $mysqli->query(
 	"SELECT * FROM `title` WHERE url = '$url_parse[0]'"
 );
-while($row = $result->fetch_row())
+while ($row = $result->fetch_row())
 	$title = $row[2];
 
 if ($_SERVER["REQUEST_URI"] == "/")
@@ -40,29 +40,30 @@ else
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<?php if (isset($div["/"])){ ?>
+<?php if (isset($div["/"])) { ?>
 
-<body style="padding-top: 70px">
-	<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand" href="#">Navbar</a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-		  <div class="collapse navbar-collapse" id="navbarSupportedContent1">
-		    <ul class="navbar-nav mr-auto">
-		      <li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> </li>
-		      <li class="nav-item"> <a class="nav-link" href="#">Link</a> </li>
-		      <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Dropdown </a>
-		        <div class="dropdown-menu" aria-labelledby="navbarDropdown1"> <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a>
-		          <div class="dropdown-divider"></div>
-		          <a class="dropdown-item" href="#">Something else here</a> </div>
-	          </li>
-		      <li class="nav-item"> <a class="nav-link disabled" href="#">Disabled</a> </li>
-	        </ul>
-		    <form class="form-inline my-2 my-lg-0">
-		      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-		      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-	        </form>
-	  </div>
-	</nav>
-</body>
+	<body style="padding-top: 70px">
+		<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand" href="#">Navbar</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent1">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> </li>
+					<li class="nav-item"> <a class="nav-link" href="#">Link</a> </li>
+					<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Dropdown </a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown1"> <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Something else here</a>
+						</div>
+					</li>
+					<li class="nav-item"> <a class="nav-link disabled" href="#">Disabled</a> </li>
+				</ul>
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				</form>
+			</div>
+		</nav>
+	</body>
 
 <?php } ?>
 <?php if (isset($div["admin"])) {
@@ -102,27 +103,28 @@ else
 
 	<body>
 		<div class="center">
-		<h4 class="mb-4 col-6 mx-auto">Se connecter à l'espace du personnel</h4>
-		<div class="col-6 mx-auto mb-4">
-			<form action=" " method="post">
-			<div class="form-group">
+			<h4 class="mb-4 col-6 mx-auto">Se connecter à l'espace du personnel</h4>
+			<div class="col-6 mx-auto mb-4">
+				<form action=" " method="post">
+					<div class="form-group">
 						<label for="identifiant">Identifiant</label>
-						<input type="identifiant" class="form-control" id="identifiant" placeholder="Identifiant" name="identifiant"> </div>
-						<div class="form-group">
-							<label for="mdp">Mot de passe</label>
-							<input type="mdp" class="form-control" id="mdp" placeholder="Mot de passe" name="mdp">
-						</div>
-				<input type="submit" class="btn btn-primary" value="Login">
-				<br><br>
-				<?php if (isset($_POST["identifiant"]) && isset($_POST["mdp"])) { ?>
-					<div class="alert">
-						<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
-						Le mot de passe ou l'identifiant n'est pas correct.
+						<input type="identifiant" class="form-control" id="identifiant" placeholder="Identifiant" name="identifiant">
 					</div>
-					<br>
-				<?php } ?>
-			</form>
-		</div>
+					<div class="form-group">
+						<label for="mdp">Mot de passe</label>
+						<input type="mdp" class="form-control" id="mdp" placeholder="Mot de passe" name="mdp">
+					</div>
+					<input type="submit" class="btn btn-primary" value="Login">
+					<br><br>
+					<?php if (isset($_POST["identifiant"]) && isset($_POST["mdp"])) { ?>
+						<div class="alert">
+							<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+							Le mot de passe ou l'identifiant n'est pas correct.
+						</div>
+						<br>
+					<?php } ?>
+				</form>
+			</div>
 	</body>
 <?php } ?>
 
