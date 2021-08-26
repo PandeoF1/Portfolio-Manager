bdfebaa7cb7046d4cf9fd3a8175bd53c1cde2c09
Nard <theo.nard18@gmail.com>
2021-08-26 10:12:14 +0200

    update

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$mysqli = new mysqli("pandeo.fr", "xxloubexx", "xxloubexx", "xxloubexx");
+$result = $mysqli->query("SELECT * FROM test");
+$row	= $result->fetch_row();
+foreach($row as $row_)
+	echo $row_;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Test</title>
+</head>
+<body>
+<p>Testeuhhh</p>
+</body>
+</html>
