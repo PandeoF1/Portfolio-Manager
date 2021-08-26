69ea485a5be7ce3b695ed8ab584d4d054588c4af
Clément Planque <clementplanque@gmail.com>
2021-08-26 18:12:03 +0200

    dummy

--- index.php
+++ index.php
@@ -42,6 +42,7 @@ else
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="/css/scrollbar.css" rel="stylesheet">
 </head>
 <?php if (isset($div["/"])) { ?>
 
@@ -99,7 +100,6 @@ else
 		<head>
 			<meta charset="utf-8">
 			<link rel="stylesheet" href="/css/login.css">
-			<link href="/css/scrollbar.css" rel="stylesheet">
 		</head>
 
 		<div class="center">
