fbc6b86cc719b86ee64cbde47ab2ccdac59fdfa8
Nard <theo.nard18@gmail.com>
2021-08-26 10:31:44 +0200

    db_access

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
+<?php
+require "config/ticket.conf.php";
+
+$mysqli = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+$result = $mysqli->query("SELECT * FROM test");
+$row	= $result->fetch_row();
+foreach($row as $row_)
+	echo $row_;
+echo "<br>url : ";
+$url_parse = explode("/", trim($_SERVER["REQUEST_URI"], "/"));
+echo $_SERVER["REQUEST_URI"];
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Portfolio PLANQUE Clément</title>
+	<title>Test</title>
 </head>
 <body>
-
+<p>Testeuhhh</p>
 </body>
 </html>
-
-<?
-
-?>
