57f56748809f06fd2ac0455dce179528d2a60985
Clément Planque <clementplanque@gmail.com>
2021-08-26 21:00:34 +0200

    index-ui

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ else
 
 	<body style="padding-top: 70px; background-image: url('/images/background.webp'); background-repeat: no-repeat; background-size: 100%; background-color: #333333;">
 		<nav>
-			
+			<p style="color: #16A7F2">#16A7F2</p>
 		</nav>
 		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 	</body>
