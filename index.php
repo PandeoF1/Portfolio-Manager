63eff3cb2833c9bdcf10773f8b39bdd1e5f602dd
Clément Planque <clementplanque@gmail.com>
2021-08-26 21:54:23 +0200

    404*

--- index.php
+++ index.php
@@ -168,7 +168,6 @@ else
         <footer class="bg-white sticky-footer">
 
         </footer>
-    </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/chart.min.js"></script>
