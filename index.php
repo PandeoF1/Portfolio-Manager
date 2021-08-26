fc586068003d3a7ddf1c42325809e4d5810a54a5
Clément Planque <clementplanque@gmail.com>
2021-08-26 13:53:16 +0200

    navbar

--- index.php
+++ index.php
@@ -48,9 +48,6 @@ else
 	<script src="style/bootstrap.bundle.min.js"></script>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php echo "Portfolio PLANQUE Clement" ?></title>
-	<link src="/style/bootstrap.min.css" rel="stylesheet" />
-	<script src="/style/bootstrap.bundle.min.js"></script>
 </head>
 <?php if (isset($div["/"])){ ?>
 
