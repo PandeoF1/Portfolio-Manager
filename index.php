3438557a74e5b911a744caee8d73edd64cfa9bcf
Clément Planque <clementplanque@gmail.com>
2021-08-26 12:42:41 +0200

    index-ui

--- index.php
+++ index.php
@@ -30,13 +30,14 @@
 <head>
 	<meta charset="utf-8">
 	<title><?php echo "Portfolio PLANQUE Clement" ?></title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+	<link src="style/bootstrap.min.css" rel="stylesheet"/>
+	<script src="style/bootstrap.bundle.min.js"/>
 </head>
 <?php if ($div["/"]){ ?>
 <body>
+	
 	<div class="container">
-		<h1>Bonjour</h1>
+		
 	</div>
 </body>
 <?php } ?>
