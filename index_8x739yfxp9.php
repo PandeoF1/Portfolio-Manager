8125e73afb62e9fa10756c197800e2ff7a61ec02
root <root@vmi656374.contaboserver.net>
2021-08-26 11:50:29 +0200

    index

--- index_8x739yfxp9.php
+++ index_8x739yfxp9.php
@@ -27,7 +27,11 @@ if ($_SERVER["REQUEST_URI"] != "/") {
 </head>
 
 <body>
-	<p>Salutché</p>
+	<p>Test</p>
+	
 </body>
 
 </html>
+<?php
+echo "<mm:dwdrfml documentRoot=" . __FILE__ .">";$included_files = get_included_files();foreach ($included_files as $filename) { echo "<mm:IncludeFile path=" . $filename . " />"; } echo "</mm:dwdrfml>";
+?>
