b9cfcd40e30e358c374f53c41e524623c1bc436f
Clément Planque <clementplanque@gmail.com>
2021-08-26 21:52:42 +0200

    403 - not found

--- index.php
+++ index.php
@@ -138,43 +138,44 @@ else
 <?php if (isset($div["error"])) { ?>
 
 	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-		<title>Not found</title>
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
-		<link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
-		<link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-		<link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
-		<link rel="stylesheet" href="assets/css/untitled-1.css">
-		<link rel="stylesheet" href="assets/css/untitled.css">
-	</head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Not found</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/untitled-1.css">
+    <link rel="stylesheet" href="assets/css/untitled.css">
+</head>
 
-	<body id="page-top">
-		<div id="wrapper">
+<body id="page-top">
+    <div id="wrapper">
+        
 
+            </div>
+            </nav>
+            <div class="container-fluid">
+                <div class="text-center mt-5">
+                    <div class="error mx-auto" data-text="404">
+                        <p class="m-0">404</p>
+                    </div>
+                    <p class="text-dark mb-5 lead">Page Not Found</p>
+                    <p class="text-black-50 mb-0">It looks like you found a glitch in the matrix...</p><a href="./login.php">← Back to Login</a></div>
+            </div>
+        </div>
+        <footer class="bg-white sticky-footer">
 
-		</div>
-		</nav>
-		<div class="container-fluid">
-			<div class="text-center mt-5">
-				<div class="error mx-auto" data-text="404">
-					<p class="m-0">404</p>
-				</div>
-				<p class="text-dark mb-5 lead">Page Not Found</p>
-				<p class="text-black-50 mb-0">It looks like you found a glitch in the matrix...</p><a href="/">← Back to index</a>
-			</div>
-		</div>
-		</div>
-		<footer class="bg-white sticky-footer">
-
-		</footer>
-		</div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
-		<script src="assets/js/jquery.min.js"></script>
-		<script src="assets/js/chart.min.js"></script>
-		<script src="assets/js/bs-init.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
-		<script src="assets/js/theme.js"></script>
-	</body>
+        </footer>
+    </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/chart.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+    <script src="assets/js/theme.js"></script>
+</body>
 <?php } ?>
 
 </html>
