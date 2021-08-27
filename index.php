5d1a8eea6f040f4188d0e703e0d568a1a4f40082
Clément Planque <clementplanque@gmail.com>
2021-08-27 17:38:28 +0200

    hover

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ else
 			<p class="text">
 				Hello, my name is PLANQUE Clément. I'm a french student in IT sector...
 			</p>
-			<button class="footer-btn" onclick="showhide();" class="btn btn-primary">Click me !</button>
+			<button class="btn btn-primary">Hover me !</button>
 		</div>
 		<footer>
 			<div class="roww">
