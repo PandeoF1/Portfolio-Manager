8731b6dd7e6abec3d686dee7a1e9d784513a04af
Clément Planque <clementplanque@gmail.com>
2021-08-27 18:11:10 +0200

    working ?

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-const footer = document.getElementById("footer");
 function displaychange() {
-  if (footer.style.display === "none") {
-    footer.style.display = "block";
-  } else {
-    footer.style.display = "none";
-  }
+    document.getElementById("footer").style.display = document.getElementById("footer").style.display === "none" ? 'block' : 'none';
 }
